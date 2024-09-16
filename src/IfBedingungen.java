83bab082042229e64c1a17345f81876665f9d97f
büsra <buesra.kablan@student.htldornbirn.at>
2024-09-16 09:03:23 +0200

    Working on Ifbedingungen

---
+++ src/IfBedingungen.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class IfBedingungen {
+    public static void main(String[] args) {
+        // Zufallszahlen erstellen
+        Random random = new Random();
+        int randomnumber = random.nextInt()
+    }
+}
