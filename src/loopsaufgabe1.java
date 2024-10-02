5225f67f0bdba767e1225dcc8a5578e231ef2f4b
büsra <buesra.kablan@student.htldornbirn.at>
2024-10-02 09:27:03 +0200

    finished loopsaufgabe1

---
+++ src/loopsaufgabe1.java
@@ -0,0 +1,14 @@
+public class loopsaufgabe1 {
+    public static void main(String[] args) {
+
+        //Zähle in einem For-Loop die Zahlen von 1 bis 100 (inklusive) zusammen
+
+        int sum= 0;
+        for (int i = 1; i <= 100; i++) {
+         sum= sum+ i;
+        }
+        System.out.println(sum);
+
+
+    }
+}
