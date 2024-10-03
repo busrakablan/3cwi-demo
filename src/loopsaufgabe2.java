40fb75dee126bf1ade7f372591c3ed34ad0b8183
büsra <buesra.kablan@student.htldornbirn.at>
2024-10-03 08:13:20 +0200

    finished with loopsaufgabe2

--- src/loopsaufgabe2.java
+++ src/loopsaufgabe2.java
@@ -1,5 +1,12 @@
 public class loopsaufgabe2 {
     public static void main(String[] args) {
 
+        //Zähle die geraden Ziffern zwischen 1 und 1000 zusammen - Tip: Starte den Loop bei 2 und erhöhe den Zählindex jeweils um 2.
+
+       int sum = 0;
+        for (int i = 2;i<=1000; i=i+2) {
+           sum=sum+i;
+        }
+        System.out.println(sum);
     }
 }
