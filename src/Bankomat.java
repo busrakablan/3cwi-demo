c623875ef866c628cab8349ab23467444091067c
büsra <buesra.kablan@student.htldornbirn.at>
2024-10-09 08:41:59 +0200

    beginn with Bankomat

--- src/Bankomat.java
+++ src/Bankomat.java
@@ -1,2 +1,10 @@
 public class Bankomat {
+    public static void main(String[] args) {
+        //Diese Aufgabe setzt sich aus allen gelernten Inhalten zusammen!
+        //Erstelle ein Programm, das einen Bankomaten simuliert. Folgende Möglichkeiten gibt es:
+        // 1. Einzahlen
+        //2. Abheben
+        //3. Kontostand
+        //4. Beenden
+    }
 }
