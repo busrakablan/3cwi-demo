f2a0b63c166001d87ed4b3c3c959e1817882a1ab
büsra <buesra.kablan@student.htldornbirn.at>
2024-10-17 09:14:51 +0200

    finished with arrays

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,6 +1,24 @@
 public class Arrays {
     public static void main(String[] args) {
 
+        int[] throwsArray = new int[6];
+        System.out.println(throwsArray[4]);
 
+        throwsArray[4] = 9;
+        for (int i = 0; 1< throwsArray.length; i++){
+            System.out.print(throwsArray[i] + " ");
+
+            int[][] playground = new int [3][3];
+            playground[0][0] = 1;
+            playground[2][1] = 2;
+
+            for (int row = 0; i < playground.length; row++) {
+                for (int col = 0; col < playground.length; col++) {
+                    System.out.print(playground[row][col] + " ");
+                }
+
+                System.out.println();
+            }
+        }
     }
 }
