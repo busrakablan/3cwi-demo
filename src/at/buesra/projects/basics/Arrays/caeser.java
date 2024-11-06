19ac44e60fbe137fe3559b423b0bb81eb0ee69ba
büsra <buesra.kablan@student.htldornbirn.at>
2024-11-06 09:11:25 +0100

    finished with caeser

--- src/at/buesra/projects/basics/Arrays/caeser.java
+++ src/at/buesra/projects/basics/Arrays/caeser.java
@@ -2,24 +2,43 @@ package at.buesra.projects.basics.Arrays;
 
 public class caeser {
     public static void main(String[] args) {
-    String result = encrypt("abc");
+        String result = encrypt("xyz");
         System.out.println(result);
+
+
+        String res = decrypt(result);
+        System.out.println(res);
         //expected cde
     }
 
     public static String encrypt(String data) {
         char[] letters = data.toCharArray();
-        for (int i = 0; i < data.length() +2; i++) {
-
+        for (int i = 0; i < data.length(); i++) {
+            letters[i] = (char) (letters[i] + 2);
+            if (letters[i] > 122) {
+                letters[i] = (char) (letters[i] - 26);
+            }
         }
-        return "";
-        System.out.println();
 
+
+        String newString = new String(letters);
+        return newString;
 
 
     }
 
     public static String decrypt(String data) {
-        return"";
+
+        char[] letters = data.toCharArray();
+        for (int i = 0; i < data.length(); i++) {
+            letters[i] = (char) (letters[i] - 2);
+            if (letters[i] < 97) {
+                letters[i] = (char) (letters[i] + 26);
+
+
+            }
+        }
+        String newString = new String(letters);
+        return newString;
     }
 }
