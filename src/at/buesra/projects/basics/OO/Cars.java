2e2f36deec5e8ee5209fd8e90383e1746bfee77e
büsra <buesra.kablan@student.htldornbirn.at>
2024-11-28 09:29:47 +0100

    finished with oo-4

--- src/at/buesra/projects/basics/OO/Cars.java
+++ src/at/buesra/projects/basics/OO/Cars.java
@@ -12,33 +12,40 @@ public class Cars {
     public int amountOfRepetitions;
     public int getRemainingRange;
 
+    public  Cars(int cS, String carColor, String carSerialNumber ){
+    this.carSpiritverbrauch = cS;
+    this.carcolor=carColor;
+    this.carserialNumber=carSerialNumber;
+    }
+
     //Methode
     public void drive() {
         this.fuelAmount = this.fuelAmount
                 - carSpiritverbrauch;
-        System.out.println("I'm driving");
+        //System.out.println("I'm driving");
     }
 
     public void dobreak() {
-        System.out.println("Ich bremse");
+
+        //System.out.println("Ich bremse");
     }
 
     public void turboBoost() {
         if (fuelAmount > carSpiritverbrauch * 0.1) {
-            System.out.println("SuperBoostMode");
+            //System.out.println("SuperBoostMode");
         } else {
-            System.out.println("Not enough fuel to go to Superboost");
+            //System.out.println("Not enough fuel to go to Superboost");
         }
     }
 
     public void amountOfRepetitions(int repetition) {
         for (int i = 0; i < repetition; i++) {
-            System.out.println("Tuuut");
+            //System.out.println("Tuuut");
         }
     }
 
     public void getRemainingRange(){
         getRemainingRange=carSpiritverbrauch/fuelAmount;
-        System.out.println(getRemainingRange);
+        //System.out.println(getRemainingRange);
     }
 }
--- src/at/buesra/projects/basics/OO/DasErsteObjekt.java
+++ src/at/buesra/projects/basics/OO/DasErsteObjekt.java
@@ -6,6 +6,7 @@ public class DasErsteObjekt {
         // Wir bauen das erste Objekt. Ein Auto!
         //  Ein Auto hat Eigenschaften (Farbe, Spritverbrauch, Seriennummer) und Fähigkeiten (fahren, bremsen).
 
+        /*
         Cars c1 = new Cars();
         c1.carcolor = "blue";
         c1.carSpiritverbrauch = 8;
@@ -15,14 +16,18 @@ public class DasErsteObjekt {
         c1.turboBoost();
         c1.amountOfRepetitions(3);
         c1.getRemainingRange();
+        */
+
+        Cars c2 = new Cars(18, "blue", "1234" );
+
+        System.out.println(c2.carSpiritverbrauch);
 
 
         //System.out.println(c1.carserialNumber);
 
-        System.out.println(c1.fuelAmount);
-        c1.drive();
-        System.out.println(c1.fuelAmount);
-
+        //System.out.println(c1.fuelAmount);
+        //c1.drive();
+        //System.out.println(c1.fuelAmount);
 
 
     }
