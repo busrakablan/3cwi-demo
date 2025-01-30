3bdd755aa8cbfb12f0b31d6813e793d42b42e988
büsra <buesra.kablan@student.htldornbirn.at>
2025-01-30 08:13:32 +0100

    working on oo12

---
+++ src/at/buesra/projects/basics/OO11/Truck.java
@@ -0,0 +1,7 @@
+package at.buesra.projects.basics.OO11;
+
+public class Truck extends Car{
+    public Truck(String color, int speed, int price, int consumption, Producer producer, Engine engine) {
+        super(color, speed, price, consumption, producer, engine);
+    }
+}
